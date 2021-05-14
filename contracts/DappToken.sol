adf6fa6d78a33a1294a62bc12f166bf8af5629e1
Hyojung Kim <darkkong88@gmail.com>
2021-05-14 15:29:51 +0900

    1. Smoke Test

---
+++ contracts/DappToken.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+contract DappToken {
+    // Constructor
+    // Set the total number of tokens
+    // Read the total number of tokens
+    uint256 public totalSupply;
+
+    constructor () {
+        totalSupply = 1000000;
+    }
+}
