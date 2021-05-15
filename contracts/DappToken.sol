2ac7fefafdf5146b6e44bd41324848ec664de368
Hyojung Kim <darkkong88@gmail.com>
2021-05-15 17:06:45 +0900

    5. An Exchange-Ready ERC-20 Token

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -13,7 +13,15 @@ contract DappToken {
         uint256 _value
     );
 
+    // Approval
+    event Approval(
+        address indexed _owner,
+        address indexed _spender,
+        uint256 _value
+    );
+
     mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
 
     constructor (uint256 _initialSupply) {
         balanceOf[msg.sender] = _initialSupply;
@@ -30,4 +38,19 @@ contract DappToken {
 
         return true;
     }
+    
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+        allowance[msg.sender][_spender] = _value;
+
+        emit Approval(msg.sender, _spender, _value);
+
+        return true;
+    }
+
+    // transferFrom
 }
+
+
+
+
+
