719a5b5da423fa4408c5caee928e646b194d250a
Hyojung Kim <darkkong88@gmail.com>
2021-05-19 18:20:52 +0900

    13. Buying ERC-20 Tokens in a Crowd Sale

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,10 @@ App = {
   web3Provider: null,
   contracts: {},
   account: "0x0",
+  loading: false,
+  tokenPrice: 1000000000000000,
+  tokensSold: 0,
+  tokensAvailable: 750000,
 
   init: function () {
     console.log("App initialized...");
@@ -20,7 +24,6 @@ App = {
       );
       web3 = new Web3(App.web3Provider);
     }
-
     return App.initContracts();
   },
 
@@ -38,12 +41,43 @@ App = {
         App.contracts.DappToken.deployed().then(function (dappToken) {
           console.log("Dapp Token Address:", dappToken.address);
         });
+
+        App.listenForEvents();
         return App.render();
       });
     });
   },
 
+  // Listen for events emitted from the contract
+  listenForEvents: function () {
+    App.contracts.DappTokenSale.deployed().then(function (instance) {
+      instance
+        .Sell(
+          {},
+          {
+            fromBlock: 0,
+            toBlock: "latest",
+          }
+        )
+        .watch(function (error, event) {
+          console.log("event triggered", event);
+          App.render();
+        });
+    });
+  },
+
   render: function () {
+    if (App.loading) {
+      return;
+    }
+    App.loading = true;
+
+    const loader = $("#loader");
+    const content = $("#content");
+
+    loader.show();
+    content.hide();
+
     // Load account data
     web3.eth.getCoinbase(function (err, account) {
       if (err === null) {
@@ -51,6 +85,61 @@ App = {
         $("#accountAddress").html("Your Account: " + account);
       }
     });
+
+    // Load token sale contract
+    App.contracts.DappTokenSale.deployed()
+      .then(function (instance) {
+        dappTokenSaleInstance = instance;
+        return dappTokenSaleInstance.tokenPrice();
+      })
+      .then(function (tokenPrice) {
+        App.tokenPrice = tokenPrice;
+        $(".token-price").html(
+          web3.fromWei(App.tokenPrice, "ether").toNumber()
+        );
+        return dappTokenSaleInstance.tokensSold();
+      })
+      .then(function (tokensSold) {
+        App.tokensSold = tokensSold.toNumber();
+        $(".tokens-sold").html(App.tokensSold);
+        $(".tokens-available").html(App.tokensAvailable);
+
+        const progressPercent = (App.tokensSold / App.tokensAvailable) * 100;
+        $("#progress").css("width", progressPercent + "%");
+
+        // Load token contract
+        App.contracts.DappToken.deployed()
+          .then(function (instance) {
+            dappTokenInstance = instance;
+            return dappTokenInstance.balanceOf(App.account);
+          })
+          .then(function (balance) {
+            $(".dapp-balance").html(balance.toNumber());
+
+            App.loading = false;
+            loader.hide();
+            content.show();
+          });
+      });
+  },
+
+  buyTokens: function () {
+    $("#content").hide();
+    $("#loader").show();
+    const numberOfTokens = $("#numberOfTokens").val();
+    App.contracts.DappTokenSale.deployed()
+      .then(function (instance) {
+        return instance.buyTokens(numberOfTokens, {
+          from: App.account,
+          value: numberOfTokens * App.tokenPrice,
+          gas: 500000,
+        });
+      })
+      .then(function (result) {
+        console.log("Tokens bought...");
+        $("form").trigger("reset"); // reset number of tokens in form
+        // Wait for Sell event
+      });
   },
 };
 
